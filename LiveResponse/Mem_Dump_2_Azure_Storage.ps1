8cb7cf24bddb33b252d284cfcae179bd3c18346c
cventour <ventouris@gmail.com>
2020-05-17 13:51:32 +0400

    Update Mem_Dump_2_Azure_Storage.ps1

--- LiveResponse/Mem_Dump_2_Azure_Storage.ps1
+++ LiveResponse/Mem_Dump_2_Azure_Storage.ps1
@@ -8,7 +8,7 @@
 ## 13/05/2020 - V1 - Initial release
 ## 15/05/2020 - V2 - Added Disk Space and Metered Connection checks
 ## 16/05/2020 - V3 - Added execution status messages. Added variables for Azure Storage Account information
-
+## 17/05/2020 - V4 - Creation of dump independent of connection type.
 
 function Write-Status($text) {
     write-host "[*] " -ForegroundColor Green -NoNewline
@@ -19,6 +19,8 @@ function Write-Status($text) {
 $AzStorageName = "STORAGEACCOUNT"  # Example "test" for test.blob.core.windows.net
 $AzContainerName = "CONTAINER" 
 $AzSAStoken = "YOUR SAS TOKEN"
+
+$DiskOverhead = 1.2. # 20% overhead assumed for disk space calculation.
 
 $AzureURI = "https://"+$AzStorageName".blob.core.windows.net/"+$AzContainerName+"?" + $AzSAStoken # DO NOT TOUCH THIS LINE
 
@@ -37,15 +39,18 @@ $connection = [Windows.Networking.Connectivity.NetworkInformation]::GetInternetC
 $isMetered = $Connection.ApproachingDataLimit -or $cost.OverDataLimit -or $cost.Roaming -or $cost.BackgroundDataUsageRestricted -or ($cost.NetworkCostType -ne "Unrestricted")
 
 if ($isMetered -eq $True) {
-    Write-Host ("[!] Connection is Metered. Exiting ...") -ForegroundColor Red
-    return
+    Write-Host ("[!] Connection is Metered. Dump will not be uploaded") -ForegroundColor Red   
     }
+else {
+    Write-Status "Connection is not metered. Dump will be uploaded to Azure"
+    }
+    
 # Check if diskspace is Available
 write-status "Checking for Memory & Available Disk Space"
 $Memsize=(Get-WmiObject win32_physicalmemory).capacity -as [long]
 $DiskFree=(Get-WmiObject win32_logicaldisk -Filter "DeviceID='C:'").freespace -as [long]
 
-if ($Diskfree -le $Memsize * 1.2) {
+if ($Diskfree -le $Memsize * $DiskOverhead) {
     Write-host ("[!] Not Enough Disk Space. Exiting ...") -ForegroundColor Red
     return
     }
@@ -72,6 +77,14 @@ Set-Location -Path C:\Windows\Temp\AzCopy\azcopy_windows_amd64*
 # Copy generated dump into Azure Storage using a limited SAS
 # It's recommended to minimize the available services and permissions of the SAS Token. Ideally the token should just provide Blob services and Write permissions!!
 # PLEASE MAKE SURE TO REPLACE THE FIELDS WITH YOUR INFO
-write-status "Copying memory dump to your container " $AzContainerName
-.\azcopy.exe copy "C:\Windows\Temp\$filename" $AzureURI 2>&1 
+
+if ($isMetered -eq $False) {
+    write-status "Copying memory dump to your container " $AzContainerName
+    .\azcopy.exe copy "C:\Windows\Temp\$filename" $AzureURI 2>&1 
+    }
+else {
+    Write-Host "[!] Dump is created but  not uploaded. Metered connection detected." -ForegroundColor Yellow
+    Write-Host "[!] Dump is located at C:\Windows\Temp\$filename" -ForegroundColor Yellow 
+    }
+    
 write-status "Execution Completed"
